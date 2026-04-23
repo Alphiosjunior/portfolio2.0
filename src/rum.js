7d967a5db836319c84a02b7b88f18602ac1349f4
Alphiosjunior <140497678+Alphiosjunior@users.noreply.github.com>
2026-04-23 13:43:13 +0200

    fix: update RUM app monitor ID to Iviwe

--- src/rum.js
+++ src/rum.js
@@ -1,7 +1,7 @@
 import { AwsRum } from 'aws-rum-web';
 
 try {
-  new AwsRum('614331ad-3712-4960-8edd-48280adc43b6', '1.0.0', 'eu-north-1', {
+  new AwsRum('94da366e-9438-42ba-a212-72a3b9b116c3', '1.0.0', 'eu-north-1', {
     sessionSampleRate: 1,
     identityPoolId: 'eu-north-1:7ab3b592-6b11-4dea-8881-359700f93aae',
     endpoint: 'https://dataplane.rum.eu-north-1.amazonaws.com',
